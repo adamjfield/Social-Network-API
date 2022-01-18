f50cdf045229962f6620f292dab4c3a95723ca01
Adam Field <adamjfield@gmail.com>
2022-01-17 22:01:47 -0600

    added reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,32 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ReactionSchema = new Schema();
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => {
+        new Types.ObjectId();
+      },
+    },
+    reactionBody: {
+      type: String,
+    },
+    username: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 const ThoughtSchema = new Schema(
   {
