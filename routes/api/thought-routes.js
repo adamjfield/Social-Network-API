0db985a396a949f7ffde7dccd5ded7d78ed1e32e
Adam Field <adamjfield@gmail.com>
2022-01-18 21:56:43 -0600

    added thought model and crud routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
 
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  removeThought,
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getAllThoughts);
+
+router.route('/:id').get(getThoughtById).put(updateThought);
+
+router.route('/:userId').post(createThought);
+
+router.route('/:userId/:thoughtId').delete(removeThought);
+
 module.exports = router;
